a86b151d3cd69d5360cdd3079a1ab4cdc80c9608
DanielB566 <danib7382@gmail.com>
2024-10-25 10:22:28 -0400

    linked AJAX

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    const btn = $("#sumbitDay");
+
+    btn.on('click', function () {
+        $.ajax({
+            url: `https://www.npoint.io/docs/e0e0a960103ef64e67c7`,
+            method: 'GET',
+            success: function (data) {
+                const schedule = data.schedule
+                const daySchedule = schedule.filter(item => 
+                    item.days.includes(selectedDay)
+                )
+    
+            },
+            error: function () {
+                alert("Theres a connection error");
+            }
+        });
+    })
+})

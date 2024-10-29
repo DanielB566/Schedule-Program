f185f049bfc746d1c6121abf0c6ddf5eae31d01b
DanielB566 <danib7382@gmail.com>
2024-10-29 12:33:45 -0400

    !!!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,68 +1,63 @@
 $(document).ready(function () {
-    
+  
+    const scheduleUrl = 'https://api.npoint.io/e0e0a960103ef64e67c7'
+  
+
     const bellSchedule = {
-        1: { start: '8:24 AM', end: '9:31 AM' },
-        2: { start: '9:36 AM', end: '10:43 AM' },
-        3: { start: '10:48 AM', end: '11:55 AM' },
-        4: { start: '12:41 PM', end: '1:48 PM' },
-        5: { start: '1:53 PM', end: '3:00 PM' }
-    };
+      1: { start: '8:24 AM', end: '9:31 AM' },
+      2: { start: '9:36 AM', end: '10:43 AM' },
+      3: { start: '10:48 AM', end: '11:55 AM' },
+      4: { start: '12:41 PM', end: '1:48 PM' },
+      5: { start: '1:53 PM', end: '3:00 PM' }
+    }
   
+
     $('#submitDay').on('click', function () {
-        const selectedDay = $('#dayInput').val().toUpperCase();
+      const selectedDay = $('#dayInput').val().trim().toUpperCase()
+  
+ 
+      if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
+        alert('Please enter a valid day (A-G)')
+        return
+      }
+  
+      // Make AJAX request to load the schedule
+      $.ajax({
+        url: scheduleUrl,
+        method: 'GET',
+        success: function (data) {
+          const schedule = data.schedule
+          const daySchedule = schedule.filter(item =>
+            item.days.includes(selectedDay)
+          )
+  
+         
+          $('#scheduleList').empty()
 
-        if (!['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(selectedDay)) {
-            alert('Please enter a valid day (A-G)');
-            return;
+          if (daySchedule.length === 0) {
+            $('#scheduleList').append(
+              '<tr><td colspan="5">No classes today.</td></tr>'
+            )
+          } else {
+        
+            daySchedule.forEach(classItem => {
+              const period = classItem.period
+              const time = bellSchedule[period] 
+              $('#scheduleList').append(`
+                              <tr>
+                                  <td>${period}</td>
+                                  <td>${time.start} - ${time.end}</td>
+                                  <td>${classItem.class}</td>
+                                  <td>${classItem.teacher}</td>
+                                  <td>${classItem.room}</td>
+                              </tr>
+                          `)
+            })
+          }
+        },
+        error: function () {
+          alert('Error loading Schedule.')
         }
-  
-        $.ajax({
-            url: `https://api.npoint.io/e0e0a960103ef64e67c7`,
-            method: 'GET',
-            success: function (data) {
-                const schedule = data.schedule;
-                const daySchedule = schedule.filter(item =>
-                    item.days.includes(selectedDay)
-                );
-  
-                $('#scheduleList').empty();
-
-                if (daySchedule.length === 0) {
-                    $('#scheduleList').append(
-                        '<tr><td colspan="5">No classes today.</td></tr>'
-                    );
-                } else {
-
-                    daySchedule.forEach(classItem => {
-                        const period = classItem.period;
-                        const time = bellSchedule[period]; 
-  
-                  
-                        if (time) {
-                            $('#scheduleList').append(`
-                                <tr>
-                                    <td>${period}</td>
-                                    <td>${time.start} - ${time.end}</td>
-                                    <td>${classItem.class}</td>
-                                    <td>${classItem.teacher}</td>
-                                    <td>${classItem.room}</td>
-                                </tr>
-                            `);
-                        } else {
-                           
-                            $('#scheduleList').append(`
-                                <tr>
-                                    <td>${period}</td>
-                                    <td colspan="4">No scheduled time available</td>
-                                </tr>
-                            `);
-                        }
-                    });
-                }
-            },
-            error: function () {
-                alert('Error loading schedule. Please check your JSON file URL.');
-            }
-        });
-    });
-});
+      })
+    })
+  })

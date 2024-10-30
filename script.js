25820416a8d14d1abdcd812a15539c8ed6e89372
DanielB566 <danib7382@gmail.com>
2024-10-30 10:43:23 -0400

    ):

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
   
-    const scheduleUrl = 'https://api.npoint.io/e0e0a960103ef64e67c7'
+    const scheduleUrl = 'schedule.json'
   
 
     const bellSchedule = {
       1: { start: '8:24 AM', end: '9:31 AM' },
       2: { start: '9:36 AM', end: '10:43 AM' },
       3: { start: '10:48 AM', end: '11:55 AM' },
-      4: { start: '12:41 PM', end: '1:48 PM' },
-      5: { start: '1:53 PM', end: '3:00 PM' }
+      lunch: { start: '11:55 AM', end: '12:35 AM'},
+      4: { start: '12:00 PM', end: '12:35 PM' },
+      5: { start: '12:41 PM', end: '1:48 PM' }
+    }
+
+    const letterDayPeriods = {
+      A: [1,2,3, "Lunch", 5,6],
+      B: [4,1,2, "Lunch", 7,5],
+      C: [3,4,1, "Lunch", 6,7],
+      D: [2,3,4, "Lunch", 5,6],
+      E: [1,2,3, "Lunch", 7,5],
+      F: [4,1,2, "Lunch", 6,7],
+      G: [3,4,7, "Lunch", 5,6]
     }
   
 
@@ -33,6 +44,8 @@ $(document).ready(function () {
   
          
           $('#scheduleList').empty()
+
+          
 
           if (daySchedule.length === 0) {
             $('#scheduleList').append(
